0671c86e10fb83a9ab1af198768c510bbaefb303
0xRavy <82102020+0xravy@users.noreply.github.com>
2024-02-27 19:14:23 +0300

    Update gruvbox.nix

--- config/colorschemes/gruvbox.nix
+++ config/colorschemes/gruvbox.nix
@@ -1,1 +1,11 @@
-
+{
+  colorschemes = {
+    gruvbox = {
+      enable = true;
+      style = "main"; #  "main", "moon", "dawn" or raw lua code
+      disableItalics = false;
+      transparentFloat = true;
+      transparentBackground = true;
+    };
+  };
+}
