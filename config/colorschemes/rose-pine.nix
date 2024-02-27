d3b149f582d7b42b52ed020768c6e5712d928161
0xRavy <82102020+0xravy@users.noreply.github.com>
2024-02-27 19:20:02 +0300

    Update rose-pine.nix

--- config/colorschemes/rose-pine.nix
+++ config/colorschemes/rose-pine.nix
@@ -1,7 +1,7 @@
 {
   colorschemes = {
     rose-pine = {
-      enable = false;
+      enable = true;
       style = "main"; #  "main", "moon", "dawn" or raw lua code
       disableItalics = false;
       transparentFloat = true;
